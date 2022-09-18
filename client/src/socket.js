f64dd2d78a6065d6d347b701124393267780576c
Tuong Tran <trangiatuong999@gmail.com>
2022-09-17 20:12:56 -0400

    change url

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-const URL = 'http://localhost:8000'
+const URL = 'https://tuongtran-chatapp.herokuapp.com/'
 const socket = io(URL, { autoConnect: false })
 
 socket.onAny((event, ...args) => {
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,9 +1,10 @@
-export const host = "http://localhost:8000"
+// export const host = "http://localhost:8000"
+export const host = "https://tuongtran-chatapp.herokuapp.com/api"
 export const loginRoute = "/auth/login";
 export const registerRoute = "/auth/register"
-export const getAllUsers = `${host}/api/auth/allUsers`
-export const getUser = `${host}/api/auth/getUser`
+export const getAllUsers = `${host}/auth/allUsers`
+export const getUser = `${host}/auth/getUser`
 export const getAllMessages = '/messages/getmsg'
 export const addMessage = '/messages/addmsg'
-export const setAva = `${host}/api/auth/setAvatar`
-export const logout = `${host}/api/auth/logout`
+export const setAva = `${host}/auth/setAvatar`
+export const logout = `${host}/auth/logout`
