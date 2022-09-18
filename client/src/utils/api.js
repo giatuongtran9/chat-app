992b1f6864e1cc060555d02c46de30e58170ef77
Tuong Tran <trangiatuong999@gmail.com>
2022-09-17 20:16:36 -0400

    fix url

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,10 +1,10 @@
 // export const host = "http://localhost:8000"
 export const host = "https://tuongtran-chatapp.herokuapp.com/api"
-export const loginRoute = "/auth/login";
-export const registerRoute = "/auth/register"
+export const loginRoute = `${host}/auth/login`;
+export const registerRoute = `${host}/auth/register`
 export const getAllUsers = `${host}/auth/allUsers`
 export const getUser = `${host}/auth/getUser`
-export const getAllMessages = '/messages/getmsg'
-export const addMessage = '/messages/addmsg'
+export const getAllMessages = `${host}/messages/getmsg`
+export const addMessage = `${host}/messages/addmsg`
 export const setAva = `${host}/auth/setAvatar`
 export const logout = `${host}/auth/logout`
